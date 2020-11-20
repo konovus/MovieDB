438bfc42b6ad78b0acd28a7157e9a4331ff9368a
konovus <ciobancostea@gmail.com>
2020-11-20 20:06:28 +0000

    adding findbyidMethod

--- app/src/main/java/com/konovus/moviedb/MainActivity.java
+++ app/src/main/java/com/konovus/moviedb/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.Bundle;
 
 import com.konovus.moviedb.models.MovieModel;
 import com.konovus.moviedb.request.Servicey;
+import com.konovus.moviedb.response.MovieResponse;
 import com.konovus.moviedb.response.MovieSearchResponse;
 import com.konovus.moviedb.utils.Credentials;
 import com.konovus.moviedb.utils.MovieAPI;
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getRetrofitResponse();
+        findById(26389);
     }
 
     private void getRetrofitResponse(){
@@ -42,13 +44,33 @@ public class MainActivity extends AppCompatActivity {
                     List<MovieModel> movies = response.body().getMovies();
                     for(MovieModel movie : movies)
                         System.out.println("\nTitle: " + movie.getTitle()
-                                        + "\n score: " + movie.getVote_average());
+                                        + "\n score: " + movie.getVote_average()
+                                        + "\n id: " + movie.getId());
                 }
             }
 
             @Override
             public void onFailure(Call<MovieSearchResponse> call, Throwable t) {
                 System.out.println("Error - " + t.getMessage());
+            }
+        });
+    }
+    private void findById(int id){
+        MovieAPI movieAPI = Servicey.getMovieAPI();
+        Call<MovieModel> responseCall = movieAPI.searchMovieById(id, Credentials.API_Key);
+        responseCall.enqueue(new Callback<MovieModel>() {
+            @Override
+            public void onResponse(Call<MovieModel> call, Response<MovieModel> response) {
+                if(response.isSuccessful()){
+                    MovieModel movie = response.body();
+                    System.out.println("\nTitle: " + movie.getTitle()
+                            + "\n score: " + movie.getVote_average());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MovieModel> call, Throwable t) {
+
             }
         });
     }
--- app/src/main/java/com/konovus/moviedb/utils/MovieAPI.java
+++ app/src/main/java/com/konovus/moviedb/utils/MovieAPI.java
@@ -1,9 +1,12 @@
 package com.konovus.moviedb.utils;
 
+import com.konovus.moviedb.models.MovieModel;
+import com.konovus.moviedb.response.MovieResponse;
 import com.konovus.moviedb.response.MovieSearchResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface MovieAPI {
@@ -13,5 +16,10 @@ public interface MovieAPI {
             @Query("api_key") String key,
             @Query("query") String query,
             @Query("page") String page
+    );
+    @GET("movie/{movie_id}")
+    Call<MovieModel> searchMovieById(
+            @Path("movie_id") int movie_id,
+            @Query("api_key") String api_key
     );
 }

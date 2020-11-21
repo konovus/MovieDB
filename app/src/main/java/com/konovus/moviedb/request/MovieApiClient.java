92a5d78615d07800dde75541809e3cdd16488cc4
konovus <ciobancostea@gmail.com>
2020-11-21 11:16:45 +0000

    adding Executors class

--- app/src/main/java/com/konovus/moviedb/request/MovieApiClient.java
+++ app/src/main/java/com/konovus/moviedb/request/MovieApiClient.java
@@ -1,8 +1,11 @@
 package com.konovus.moviedb.request;
 
+import com.konovus.moviedb.AppExecutors;
 import com.konovus.moviedb.models.MovieModel;
 
 import java.util.List;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
@@ -25,5 +28,21 @@ public class MovieApiClient {
 
     public LiveData<List<MovieModel>> getMovies(){
         return mMovies;
+    }
+
+    public void searchMovieApi(){
+        final Future myHandler = AppExecutors.getInstance().getmNetworkIO().submit(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+
+        AppExecutors.getInstance().getmNetworkIO().schedule(new Runnable() {
+            @Override
+            public void run() {
+                myHandler.cancel(true);
+            }
+        }, 5000, TimeUnit.MICROSECONDS);
     }
 }

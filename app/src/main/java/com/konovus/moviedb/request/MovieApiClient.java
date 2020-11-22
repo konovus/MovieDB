7a3430bf100a2cbf2aa365f72bb71f77ca5f71b0
konovus <ciobancostea@gmail.com>
2020-11-22 10:03:25 +0000

    adding retrieveMoviesRunnable class

--- app/src/main/java/com/konovus/moviedb/request/MovieApiClient.java
+++ app/src/main/java/com/konovus/moviedb/request/MovieApiClient.java
@@ -2,19 +2,26 @@ package com.konovus.moviedb.request;
 
 import com.konovus.moviedb.AppExecutors;
 import com.konovus.moviedb.models.MovieModel;
+import com.konovus.moviedb.response.MovieSearchResponse;
+import com.konovus.moviedb.utils.Credentials;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import retrofit2.Call;
+import retrofit2.Response;
 
 public class MovieApiClient {
 
     private MutableLiveData<List<MovieModel>> mMovies;
 
     private static MovieApiClient instance;
+
+    private RetrieveMoviesRunnable retrieveMoviesRunnable;
 
     public static MovieApiClient getInstance(){
         if(instance == null)
@@ -30,13 +37,11 @@ public class MovieApiClient {
         return mMovies;
     }
 
-    public void searchMovieApi(){
-        final Future myHandler = AppExecutors.getInstance().getmNetworkIO().submit(new Runnable() {
-            @Override
-            public void run() {
-
-            }
-        });
+    public void searchMovieApi(String query, int pageNumber){
+        if(retrieveMoviesRunnable != null)
+            retrieveMoviesRunnable = null;
+        retrieveMoviesRunnable = new RetrieveMoviesRunnable(query, pageNumber);
+        final Future myHandler = AppExecutors.getInstance().getmNetworkIO().submit(retrieveMoviesRunnable);
 
         AppExecutors.getInstance().getmNetworkIO().schedule(new Runnable() {
             @Override
@@ -44,5 +49,53 @@ public class MovieApiClient {
                 myHandler.cancel(true);
             }
         }, 5000, TimeUnit.MICROSECONDS);
+    }
+
+    private class RetrieveMoviesRunnable implements Runnable{
+        private String query;
+        private int pageNumber;
+        boolean cancelRequest;
+
+        public RetrieveMoviesRunnable(String query, int pageNumber) {
+            this.query = query;
+            this.pageNumber = pageNumber;
+            cancelRequest = false;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Response response = getMovies(query, pageNumber).execute();
+                if(cancelRequest)
+                    return;
+                if(response.isSuccessful()){
+                    List<MovieModel> list = ((MovieSearchResponse) response.body()).getMovies();
+                    if(pageNumber == 1)
+                        mMovies.postValue(list);
+                    else{
+                        List<MovieModel> currentList = mMovies.getValue();
+                        currentList.addAll(list);
+                        mMovies.postValue(currentList);
+                    }
+                } else mMovies.postValue(null);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+
+        }
+
+        private Call<MovieSearchResponse> getMovies(String query, int pageNumber){
+            return Servicey.getMovieAPI().searchMovie(
+                    Credentials.API_Key,
+                    query,
+                    String.valueOf(pageNumber)
+            );
+        }
+
+        private void cancelRequest(){
+            cancelRequest = true;
+        }
     }
 }
